1d05b442f54be4d10b10f7fca1c34b377b4a6469
xoxoisme <kxoxo3333@gmail.com>
2025-12-16 15:26:05 +0900

    feat: Withdraw 관련 엔티티 및 상태 enum 생성

--- src/main/java/devut/buzzerbidder/domain/payment/entity/WithdrawStatus.java
+++ src/main/java/devut/buzzerbidder/domain/payment/entity/WithdrawStatus.java
@@ -1,8 +1,7 @@
 package devut.buzzerbidder.domain.payment.entity;
 
 public enum WithdrawStatus {
-    PENDING,    // 출금 진행 중
-    REJECTED,   // 출금 거부
-    APPROVED,   // 출금 승인
-    FAILED      // 출금 실패
+    REQUESTED,      // 출금 요청 완료
+    APPROVED,       // 출금 승인
+    FAILED          // 출금 실패
 }
