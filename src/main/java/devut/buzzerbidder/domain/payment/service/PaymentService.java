99563a544019ddcfffea7fd6e3d41499a2b38af7
xoxoisme <kxoxo3333@gmail.com>
2025-12-16 15:26:52 +0900

    feat: PaymentService 내 출금 기능 추가 및 TODO 완료

--- src/main/java/devut/buzzerbidder/domain/payment/service/PaymentService.java
+++ src/main/java/devut/buzzerbidder/domain/payment/service/PaymentService.java
@@ -1,24 +1,23 @@
 package devut.buzzerbidder.domain.payment.service;
 
 import devut.buzzerbidder.domain.payment.dto.PaymentHistoryItemDto;
-import devut.buzzerbidder.domain.payment.dto.request.PaymentConfirmRequestDto;
-import devut.buzzerbidder.domain.payment.dto.request.PaymentCreateRequestDto;
-import devut.buzzerbidder.domain.payment.dto.request.PaymentFailRequestDto;
-import devut.buzzerbidder.domain.payment.dto.request.PaymentHistoryRequestDto;
-import devut.buzzerbidder.domain.payment.dto.response.PaymentConfirmResponseDto;
-import devut.buzzerbidder.domain.payment.dto.response.PaymentCreateResponseDto;
-import devut.buzzerbidder.domain.payment.dto.response.PaymentFailResponseDto;
-import devut.buzzerbidder.domain.payment.dto.response.PaymentHistoryResponseDto;
+import devut.buzzerbidder.domain.payment.dto.request.*;
+import devut.buzzerbidder.domain.payment.dto.response.*;
 import devut.buzzerbidder.domain.payment.entity.Payment;
 import devut.buzzerbidder.domain.payment.entity.PaymentMethod;
 import devut.buzzerbidder.domain.payment.entity.PaymentStatus;
-import devut.buzzerbidder.domain.payment.repository.PaymentRepository;
-import devut.buzzerbidder.domain.user.entity.User;
-import devut.buzzerbidder.domain.user.repository.UserRepository;
-import devut.buzzerbidder.global.exeption.BusinessException;
-import devut.buzzerbidder.global.exeption.ErrorCode;
+import devut.buzzerbidder.domain.payment.entity.Withdraw;
 import devut.buzzerbidder.domain.payment.infrastructure.tosspayments.TossPaymentsClient;
 import devut.buzzerbidder.domain.payment.infrastructure.tosspayments.dto.response.TossConfirmResponseDto;
+import devut.buzzerbidder.domain.payment.repository.PaymentRepository;
+import devut.buzzerbidder.domain.payment.repository.WithdrawRepository;
+import devut.buzzerbidder.domain.user.entity.User;
+import devut.buzzerbidder.domain.user.repository.UserRepository;
+import devut.buzzerbidder.domain.wallet.entity.Wallet;
+import devut.buzzerbidder.domain.wallet.repository.WalletRepository;
+import devut.buzzerbidder.domain.wallet.service.WalletService;
+import devut.buzzerbidder.global.exeption.BusinessException;
+import devut.buzzerbidder.global.exeption.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -32,43 +31,50 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final UserRepository userRepository;
     private final TossPaymentsClient tossPaymentsClient;
+    private final WithdrawRepository withdrawRepository;
+    private final WalletRepository walletRepository;
+    private final WalletService walletService;
 
     @Transactional
     public PaymentCreateResponseDto create(Long userId, PaymentCreateRequestDto request) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
-        String orderId = genOrderId();
+        String orderId = genOrderId();  // 토스와 통신에 사용할 주문 ID 생성
         Long amount = request.amount();
 
-        Payment payment = Payment.create(user, orderId, "코인 충전", amount);
+        Payment payment = new Payment(user, orderId, "코인 충전", amount);
         paymentRepository.save(payment);
 
         return PaymentCreateResponseDto.from(payment);
     }
 
-    public PaymentConfirmResponseDto confirm(PaymentConfirmRequestDto request) {
+    public PaymentConfirmResponseDto confirm(Long userId, PaymentConfirmRequestDto request) {
         String paymentKey = request.paymentKey();
         String orderId = request.orderId();
         Long amount = request.amount();
 
         Payment payment = paymentRepository.findByOrderId((orderId))
                 .orElseThrow(() -> new BusinessException(ErrorCode.PAYMENT_NOT_FOUND));
-        // 상태 검증
+
+        // 이미 처리된 결제에 대해서 중복 승인 방지용
         if (!payment.isPending()) {
             throw new BusinessException(ErrorCode.NOT_PENDING_PAYMENT);
         }
-        // 금액 검증
+
+        // 요청 금액과 결제 생성금액 동일한지 확인용
         if (!payment.getAmount().equals(amount)) {
             throw new BusinessException(ErrorCode.INVALID_AMOUNT);
         }
 
         TossConfirmResponseDto response = tossPaymentsClient.confirmPayment(request);
-        // 응답상태(토스) 검증
+
+        // 토스의 응답상태 확인용
         if (!response.status().equals("DONE")) {
             throw new BusinessException(ErrorCode.PAYMENT_CONFIRM_FAILED);
         }
@@ -76,15 +82,21 @@ public class PaymentService {
         payment.confirm(
                 paymentKey,
                 PaymentMethod.fromToss(response.method()),
-                response.approvedAt());
+                response.approvedAt()
+        );
         paymentRepository.save(payment);
 
-        // TODO: 금액 재검증 후, 코인 충전 추가
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        Wallet wallet = walletService.getOrCreateWalletWithLock(user);
+        walletService.chargeBizz(user, amount);
+        walletRepository.save(wallet);
 
         return PaymentConfirmResponseDto.from(payment);
     }
 
-    public PaymentFailResponseDto fail(Long userId, PaymentFailRequestDto request) {
+    public PaymentFailResponseDto fail(PaymentFailRequestDto request) {
         String orderId = request.orderId();
         String code = request.code();
         String msg = request.msg();
@@ -110,11 +122,16 @@ public class PaymentService {
         if (endTime.isBefore(startTime)) {
             throw new BusinessException(ErrorCode.INVALID_DATE_RANGE);
         }
-        // TODO: 최대 기간 검증
 
         int page = request.page() == null ? 0 : request.page();
         int size = request.size() == null ? 10 : request.size();
-        // TODO: 최대 사이즈 검증
+
+        if (page >= 15) {
+            throw new BusinessException(ErrorCode.INVALID_PAGE_ERROR);
+        }
+        if (size > 30) {
+            throw new BusinessException(ErrorCode.INVALID_PAGE_SIZE);
+        }
 
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "approvedAt"));
         Page<Payment> paymentPage;
@@ -127,6 +144,7 @@ public class PaymentService {
                     pageable
             );
         } else {
+            // 상태 존재 시, 해당 상태로 조회
             PaymentStatus paymentStatus = PaymentStatus.valueOf(status);
             paymentPage = paymentRepository.findByUserIdAndApprovedAtBetweenAndStatus(
                     userId,
@@ -144,41 +162,30 @@ public class PaymentService {
         return PaymentHistoryResponseDto.from(payments, paymentPage);
     }
 
-//    public PaymentCancelResponseDto cancelPayment(Long userId, PaymentCancelRequestDto request) {
-//        String cancelReason = request.cancelReason();
-//        Payment payment = paymentRepository.findById(userId)
-//                .orElseThrow(() -> new BusinessException(ErrorCode.PAYMENT_NOT_FOUND));
-//
-//        // 상태 검증
-//        if (!payment.isSuccess()) {
-//            throw new BusinessException((ErrorCode.NOT_SUCCESS_PAYMENT));
-//        }
-//
-//        TossCancelResponseDto response = tossPaymentsClient.cancelPayment(payment.getPaymentKey(), request);
-//        // 응답상태(토스) 검증
-//        if (!response.status().equals("CANCELED")) {
-//            throw new BusinessException(ErrorCode.PAYMENT_CANCELED_FAILED);
-//        }
-//
-//        payment.cancel(
-//                response.cancelAmount(),
-//                cancelReason,
-//                response.canceledAt()
-//        );
-//        paymentRepository.save(payment);
-//
-//        return PaymentCancelResponseDto.from(payment);
-//    }
+    @Transactional
+    public WithdrawResponseDto withdrawPayment(Long userId, WithdrawRequestDto request) {
 
-//    public PaymentWithdrawResponseDto withdrawPayment(Long userId, PaymentWithdrawRequestDto request) {
-//        User user = userRepository.findById(userId)
-//                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
-//
-//        // TODO: 코인 잔액 검증 추가
-//        // TODO: 코인 잠그기(상태변경)
-//        // TODO: 출금 엔티티 생성 후 DB저장
-//        // TODO: 응답 DTO 연결
-//    }
+        // 동시 출금 요청 방지용(비관적 락)
+        Wallet wallet = walletRepository.findByUserIdWithLock(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.WALLET_NOT_FOUND));
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        Long amount = request.amount();
+
+        if (wallet.getBizz() < amount) {
+            throw new BusinessException(ErrorCode.BIZZ_INSUFFICIENT_BALANCE);
+        }
+
+        walletService.withdrawBizz(user, amount);
+        walletRepository.save(wallet);
+
+        Withdraw withdraw = new Withdraw(user, amount, request.bankName(), request.accountNumber(), request.accountHolder());
+        withdrawRepository.save(withdraw);
+
+        return WithdrawResponseDto.from(withdraw);
+    }
 
     private String genOrderId() {
         return "ORDER_" + System.currentTimeMillis();
