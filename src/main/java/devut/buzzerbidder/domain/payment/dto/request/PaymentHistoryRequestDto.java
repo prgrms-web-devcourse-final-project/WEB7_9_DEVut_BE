53505056592c8621fce9787af382511c093ae47a
xoxoisme <kxoxo3333@gmail.com>
2025-12-18 11:59:30 +0900

    fix: swagger Tag 작성 및 param형식으로 변경

--- src/main/java/devut/buzzerbidder/domain/payment/dto/request/PaymentHistoryRequestDto.java
+++ src/main/java/devut/buzzerbidder/domain/payment/dto/request/PaymentHistoryRequestDto.java
@@ -1,19 +1,12 @@
 package devut.buzzerbidder.domain.payment.dto.request;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Positive;
-
 import java.time.OffsetDateTime;
 
 public record PaymentHistoryRequestDto(
         OffsetDateTime startDate,
         OffsetDateTime endDate,
         String status,
-
-        @Min(0)
         Integer page,
-
-        @Positive
         Integer size
 ) {
 }
