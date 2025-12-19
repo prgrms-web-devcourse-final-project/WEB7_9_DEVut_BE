9e7a69bc9bb3770cfaf9f8f0364bcce665f3fe33
[BE9]도석환 <do04080@gmail.com>
2025-12-19 09:28:18 +0900

    feat: 내가 작성한 글 및 찜한 글 조회

--- src/main/java/devut/buzzerbidder/domain/likedelayed/repository/LikeDelayedRepository.java
+++ src/main/java/devut/buzzerbidder/domain/likedelayed/repository/LikeDelayedRepository.java
@@ -3,29 +3,12 @@ package devut.buzzerbidder.domain.likedelayed.repository;
 import devut.buzzerbidder.domain.delayeditem.entity.DelayedItem;
 import devut.buzzerbidder.domain.likedelayed.entity.LikeDelayed;
 import devut.buzzerbidder.domain.user.entity.User;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface LikeDelayedRepository extends JpaRepository<LikeDelayed, Long> {
 
     Optional<LikeDelayed> findByUserAndDelayedItem(User user, DelayedItem delayedItem);
 
     long countByDelayedItemId(Long delayedItemId);
-
-    @Query("""
-        SELECT ld.delayedItem FROM LikeDelayed ld
-        LEFT JOIN FETCH ld.delayedItem.images
-        WHERE ld.user.id = :userId
-        ORDER BY ld.createDate DESC
-        """)
-    List<DelayedItem> findLikedDelayedItemsByUserId(@Param("userId") Long userId);
-
-    @Query("""
-        SELECT COUNT(ld) FROM LikeDelayed ld
-        WHERE ld.user.id = :userId
-        """)
-    long countByUserId(@Param("userId") Long userId);
 }
