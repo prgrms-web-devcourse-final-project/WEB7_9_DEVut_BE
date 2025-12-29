a040875a9732f18e8eb951696ef0e3f23fbc7c82
premierbell <pjw319@naver.com>
2025-12-29 23:18:34 +0900

    feat: 알림 모두 읽음 처리 API 추가

--- src/main/java/devut/buzzerbidder/domain/notification/repository/NotificationRepository.java
+++ src/main/java/devut/buzzerbidder/domain/notification/repository/NotificationRepository.java
@@ -3,6 +3,9 @@ package devut.buzzerbidder.domain.notification.repository;
 import devut.buzzerbidder.domain.notification.entity.Notification;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
@@ -11,5 +14,9 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     List<Notification> findByUserIdAndIsCheckedFalseOrderByCreateDateDesc(Long userId);
 
     Long countByUserIdAndIsCheckedFalse(Long userId);
+
+    @Modifying
+    @Query("UPDATE Notification n SET n.isChecked = true WHERE n.userId = :userId AND n.isChecked = false")
+    int markAllAsReadByUserId(@Param("userId") Long userId);
 
 }
